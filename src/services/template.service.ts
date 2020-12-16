1657503b41e5f498b9b3c4d6d68bdbcc718bf84d
Viktor <tolstik.va@gmail.com>
2020-12-16 23:56:32 +0300

    save

--- src/services/template.service.ts
+++ src/services/template.service.ts
@@ -1,22 +1,39 @@
+import { MyContext } from './../types/my-context';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as handlebars from 'handlebars';
 import { readDirDeepSync } from 'read-dir-deep';
 import { Injectable, Logger } from '@nestjs/common';
 
-export interface IParams {
-	action: string;
-	status: string;
-	lang?: string;
+type SUPPORTED_LANGUAGES = 'ru';
+type TemplateResolver = (ctx: MyContext, data: any) => string;
+type LangTranslate = Map<string, TemplateResolver>;
+type TranslateMap = Record<SUPPORTED_LANGUAGES, LangTranslate>;
+
+function splitLimit(input: string, splitter: string, limit: number = -1): string[] {
+	if (limit === 0) {
+		return [input];
+	}
+
+	const index = input.indexOf(splitter);
+
+	if (index < 0) {
+		return [input];
+	}
+
+	const head = input.slice(0, index);
+	const tail = input.slice(index + splitter.length);
+
+	return [head, ...splitLimit(tail, splitter, limit - 1)];
 }
 
 @Injectable()
 export class TemplateService {
-	private readonly DEFAULT_LANG: string = 'ru';
+	private readonly DEFAULT_LANG: SUPPORTED_LANGUAGES = 'ru';
 	private readonly TEMPLATE_PATH: string = 'templates';
 
 	private logger: Logger;
-	private templatesMap: Map<string, (d: any) => string>;
+	private templatesMap: TranslateMap;
 
 	constructor(logger: Logger) {
 		this.logger = logger;
@@ -24,9 +41,9 @@ export class TemplateService {
 		this.load();
 	}
 
-	public apply(params: IParams, data: any): string {
+	public apply(key: string, data: any): string {
 		this.logger.debug(
-			`apply template: ${params.action} ${params.status} ${params.lang}`,
+			`apply template: ${key}`,
 		);
 
 		let template = this.getTemplate(params);
@@ -43,9 +60,9 @@ export class TemplateService {
 		return template(data);
 	}
 
-	private getTemplate(params: IParams): (data: any) => string {
-		const { lang, action, status } = params;
-		return this.templatesMap.get(this.getTemplateKey(lang, action, status));
+	private getTemplate(key: string): (data: any) => string {
+		const templateKey = this.getTemplateKey()
+		return this.templatesMap.get(key);
 	}
 
 	private load() {
@@ -53,26 +70,36 @@ export class TemplateService {
 			process.cwd(),
 			this.TEMPLATE_PATH,
 		);
-		const templateFileNames: string[] = readDirDeepSync(templatesDir);
+		const templateFileNames: string[] = readDirDeepSync(templatesDir, {
+			patterns: ['**/*.md'],
+		});
 
 		this.templatesMap = templateFileNames.reduce((acc, fileName) => {
 			const template = fs.readFileSync(fileName, { encoding: 'utf-8' });
 
-			const [, lang, action, status] = fileName
-				.replace(/\.hbs$/, '')
-				.split('/');
-			return acc.set(
-				this.getTemplateKey(lang, action, status),
+			const relative = path.relative(templatesDir, fileName)
+				.replace(/\\/, '/')
+				.replace(/\.md$/, '');
+
+			const [lang, key] = splitLimit(relative, '/', 1);
+
+			const langObj: LangTranslate = acc[lang] || new Map();
+
+			langObj.set(
+				key,
 				handlebars.compile(template),
 			);
-		}, new Map());
+
+			acc[lang] = langObj;
+
+			return acc;
+		}, {} as TranslateMap);
 	}
 
 	private getTemplateKey(
 		lang: string,
-		action: string,
-		status: string,
+		key: string
 	): string {
-		return `${lang}-${action}-${status}`;
+		return `${lang}/${key}`;
 	}
 }
--- src/test.ts
+++ src/test.ts
@@ -2,9 +2,27 @@ import { BotService } from './services/bot.service';
 import { AnalysisService } from './services/analysis.service';
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as path from 'path';
 
 function normalizeKey(key: string) {
 	return key ? key.replace(/[.#$/\[\]]/, '_') : key;
+}
+
+function splitLimit(input: string, splitter: string, limit: number = -1) {
+	if (limit === 0) {
+		return [input];
+	}
+
+	const index = input.indexOf(splitter);
+
+	if (index < 0) {
+		return [input];
+	}
+
+	const head = input.slice(0, index);
+	const tail = input.slice(index + splitter.length);
+
+	return [head, ...splitLimit(tail, splitter, limit - 1)];
 }
 
 async function test1() {
@@ -18,8 +36,10 @@ async function test1() {
 
 async function test() {
 
-	await test1()
+	// await test1()
 	// console.log(normalizeKey('GAZP.ME'));
+	console.log(splitLimit('level1/level2/level3', '/'));
+	// console.log(path.relative('level1', 'level1/level2/level3').replace(/\\/, '/'));
 }
 
 
